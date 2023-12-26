90ab57813cad5f95cf3f8954a530545d11c2974b
Georgy <georgy.polkanov@yandex.ru>
2023-12-26 21:49:54 +0300

    create first model

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Item:
+    name = models.CharField(max_legnth=100)
+    description = models.CharField(max_legnth=100)
+    price = models.IntegerField(max_legnth=100)
