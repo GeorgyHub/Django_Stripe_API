b1c98fe74781ff577b907f1eb551f1f8bc994255
Georgy <georgy.polkanov@yandex.ru>
2024-01-04 21:21:21 +0300

    added payments

--- backend/models.py
+++ backend/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 # Create your models here.
 class Item:
-    name = models.CharField(max_legnth=100)
-    description = models.CharField(max_legnth=100)
-    price = models.IntegerField(max_legnth=100)
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
+    price = models.IntegerField(max_length=100)
